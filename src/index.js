386cccf78f4350e437b0eed328c9d315b97d37ad
Maria <maria.rodrig92@outlook.com>
2024-04-19 19:44:10 -0400

    added content area and side menu list

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import AppThemeProvider from "@themes/AppThemeProvider";
+import CssBaseline from "@mui/material/CssBaseline";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppThemeProvider>
+      <CssBaseline />
+      <App />
+    </AppThemeProvider>
   </React.StrictMode>
 );
 
